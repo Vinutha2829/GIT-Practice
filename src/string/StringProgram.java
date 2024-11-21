2e7742c49198a8bcf047c0b27d30077fafd57c5e
Vinutha T <veenavinu29@gmail.com>
2024-11-21 23:48:45 +0530

    add code as per PR comments

--- src/string/StringProgram.java
+++ src/string/StringProgram.java
@@ -3,8 +3,15 @@ package string;
 public class StringProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Reverse a string:
+		String name="vinutha";
+		String rev="";
+		for(int i=name.length()-1;i>=0;i--)
+		{
+			rev=rev+name.charAt(i);
+		}
+		System.out.println(rev);
+		
 	}
 
 }
