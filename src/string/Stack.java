7d534ca2b25de89d0c2c724e4560b2109b1bfefc
Vinutha T <veenavinu29@gmail.com>
2024-11-22 00:28:15 +0530

    merge1

---
+++ src/string/Stack.java
@@ -0,0 +1,20 @@
+package string;
+
+public class Stack {
+
+
+		public static void main(String[] args) {
+			String s=new String("you cannot change me");
+			String s1=new String("you cannot change me");
+			String s2="you cannot change Me";
+			String s3="YOU CANNOT CHANGE mE";
+			String s4="you cannot change ME";
+			System.out.println(s2==s3);
+			System.out.println(s2==s4);
+			System.out.println(s2==s4);
+
+		}
+
+	}
+
+
